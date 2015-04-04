4deb3aa190657fd75da14a51796c5782aea6940a
Ben! <ben@bensites.com>
2015-04-04 09:10:49 -0500

    Login and pages

--- js/PlanHW.js
+++ js/PlanHW.js
@@ -1,9 +1,25 @@
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
-
+var PlanHWApi = "http://localhost:3000/"
 angular.module('PlanHW', ['ngRoute'])
-    .controller('IndexCtrl', function ($scope) {
+    .config(function($routeProvider){
+        
+        $routeProvider
+        
+        .when('/',{
+            templateUrl: 'pages/landing.html',
+            controller: 'IndexCtrl'
+        })
+        .when('/signup',{
+            templateUrl: 'pages/signup.html'
+        })
+        .when('/signin',{
+            templateUrl: 'pages/signin.html',
+            controller: 'SigninCtrl'
+        })
+        
+    }).controller('IndexCtrl', function ($scope) {
         $scope.signuplinks = true
         var body = $('body');
         var time = moment().add(1 + Math.floor(Math.random() * 6), 'days');
@@ -38,39 +54,31 @@ angular.module('PlanHW', ['ngRoute'])
 
         $scope.timeFull = time.format('dddd');
         $scope.timeFromNow = time.fromNow();
-    }).controller('SignupController', function($scope, $http){
-        $scope.signupErrored = false;
-        $scope.student = {};
-        $scope.gravatarInfo = "";
-        $('#email-field').change(function(){
-            $http.jsonp("http://www.gravatar.com/" + md5($('#email-field').val()) + ".json?callback=JSON_CALLBACK")
-                .success(function(data){
-                    $scope.student.name = data['entry'][0]['name']['givenName'];
-                    $scope.student.username = data['entry'][0]['preferredUsername'];
-                    $scope.gravatarInfo = "If you'd like to change this image, log in to Gravatar."
-                }).error(function(data,status){
-                    $scope.gravatarInfo = "If you'd like to change this image, create an account at Gravatar."
-                });
-        });
-        $scope.signup = function(student){
-            $http.post('http://localhost:3000/students/', student)
-            .success(function(){
-                alert("Welcome to PlanHW!");
-                http.get('http://localhost:3000/login?username='+ student.username +
-                    'password=' + student.password
-                ).success(function(data){
-                    
-                })
-            }).error(function(data, status){
-                $scope.signupErrored = true;
-                if(status === 422){
-                    $scope.signupErrors = data['errors'];
-                } else {
-                    $scope.signupErrors = ["Something went wrong, try again?"]
+    }).controller('SigninCtrl', function($scope, $rootScope, $http){
+        $scope.signinError = null;
+        $scope.signin = function(username,password){
+            $http.get(PlanHWApi+'login?username='+username+'&password='+password)
+            .success(function(data){
+                $rootScope.student_token = data['login']['token']
+                $rootScope.student_id = data['student']['id']
+                $http.get(PlanHWApi+"test_login?token="+$rootScope.student_token+"&id="+$rootScope.student_id)
+                    .success(function(test){
+                        if(test['correct']){
+                            
+                        } else {
+                            $scope.signinError("Something went wrong in saving authenication. This should never happen. Check the logs for more info.");
+                            console.log(data);
+                            console.log(test);
+                            $rootScope.student_token = null
+                            $rootScope.student_id = null
+                        }
+                    });
+            }).error(function(data,status){
+                if(status === 401){
+                    $scope.signinError = "Wrong username/password.";
                 }
             });
-        };
-        $scope.good_confirm = false;
+        }; 
     }).directive('gravatar', function(){return{
         restrict: 'AE',
         replace: true,
@@ -81,5 +89,44 @@ angular.module('PlanHW', ['ngRoute'])
         template: '<img alt="Your Avatar" src="https://secure.gravatar.com/avatar/{{ md5(email) }}.png?s={{ size }}&d=monsterid"/>',
         controller: function($scope){
             $scope.md5 = md5
+        }
+    }}).directive('signup', function(){return{
+        restrict: 'AE',
+        replace: true,
+        scope: {},
+        template: '<div ng-include="\'directives/signup_form.html\'"></div>',
+        controller: function($scope, $http){
+            $scope.signupErrored = false;
+            $scope.student = {};
+            $scope.gravatarInfo = "";
+            $scope.gravatarUpdate = function(){
+                $http.jsonp("http://www.gravatar.com/" + md5($scope.student.email) + ".json?callback=JSON_CALLBACK")
+                    .success(function(data){
+                        $scope.student.name = data['entry'][0]['name']['givenName'];
+                        $scope.student.username = data['entry'][0]['preferredUsername'];
+                        $scope.gravatarInfo = "If you'd like to change this image, log in to Gravatar."
+                    }).error(function(data){
+                        $scope.gravatarInfo = "If you'd like to change this image, create an account at Gravatar."
+                    });
+                },
+            $scope.signup = function(student){
+                $http.post(PlanHWApi+'students/', student)
+                .success(function(){
+                    alert("Welcome to PlanHW!");
+                    http.get(PlanHWApi+'login?username='+ student.username +
+                        'password=' + student.password
+                    ).success(function(data){
+
+                    })
+                }).error(function(data, status){
+                    $scope.signupErrored = true;
+                    if(status === 422){
+                        $scope.signupErrors = data['errors'];
+                    } else {
+                        $scope.signupErrors = ["Something went wrong, try again?"]
+                    }
+                });
+            };
+            $scope.good_confirm = false;
         }
     }});
