259417ee13166375e9f314677819b5ead8e76666
Ben! <ben@bensites.com>
2015-04-11 17:09:26 -0500

    konami and function wrap

--- js/PlanHW.js
+++ js/PlanHW.js
@@ -1,4 +1,4 @@
-function toTitleCase(str) {
+(function(){function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 var PlanHWApi = "http://localhost:3000/"
@@ -256,3 +256,6 @@ angular.module('PlanHW', ['ngRoute'])
             $scope.good_confirm = false;
         }
     }});
+
+
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65,13";$(document).keydown(function(e){kkeys.push( e.keyCode );if (kkeys.toString().indexOf( konami ) >= 0 ) {$(document).unbind('keydown',arguments.callee);alert("I like potatoes.");$("body").addClass("konami");}});})();
