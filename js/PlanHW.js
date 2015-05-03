7484043a818f103718a3f49f88165e65e124c4a2
Ben! <ben@bensites.com>
2015-05-02 22:52:28 -0500

    Better easter eggs

--- js/PlanHW.js
+++ js/PlanHW.js
@@ -402,4 +402,4 @@ angular.module('PlanHW', ['ngRoute','ui.bootstrap.datetimepicker','ngCookies','w
     }});
 
 
-var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65,13";$(document).keydown(function(e){kkeys.push( e.keyCode );if (kkeys.toString().indexOf( konami ) >= 0 ) {$(document).unbind('keydown',arguments.callee);alert("I like potatoes.");$("body").addClass("konami");}});})();
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65,13";$(document).keydown(function(e){kkeys.push( e.keyCode );if (kkeys.toString().indexOf(konami)>=0){$(document).unbind('keydown',arguments.callee);$("body").addClass("konami");}});})();
