049499c636c6151a16c32a3991f36c6fb4e46ce7
Ben! <ben@bensites.com>
2015-05-14 08:05:27 -0500

    konamisaw

--- js/PlanHW.js
+++ js/PlanHW.js
@@ -586,4 +586,4 @@ angular.module('PlanHW', ['ngRoute','ui.bootstrap.datetimepicker','webStorageMod
 ;
 })();
 
-var kk=[],k="38,38,40,40,37,39,37,39,66,65,13",doc=$(document);doc.keydown(function(e){kk.push(e.keyCode);if(kk.toString().indexOf(k)>=0){doc.unbind('keydown',arguments.callee);$("body").addClass("konami");}});function toTitleCase(s){return s.replace(/\w\S*/g,function(t){return t.charAt(0).toUpperCase()+t.substr(1).toLowerCase();});}
+var kk=[],k="38,38,40,40,37,39,37,39,66,65,13",doc=$(document);doc.keydown(function(e){kk.push(e.keyCode);if(kk.toString().indexOf(k)>=0){doc.unbind('keydown',arguments.callee);$("body");nd_mode="chainsaw";nd_vAlign="bottom";nd_hAlign="right";nd_vMargin="10";nd_hMargin="10";nd_target="_top";$("body").append('<script language="javascript" src="http://www.netdisaster.com/js/mynd.js"></script>')}});function toTitleCase(s){return s.replace(/\w\S*/g,function(t){return t.charAt(0).toUpperCase()+t.substr(1).toLowerCase();});}
