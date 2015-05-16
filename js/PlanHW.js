d6e62afa9926e347d5e78822cd4d9e89c3f6a25c
Ben! <ben@bensites.com>
2015-05-15 22:44:39 -0500

    Sign in with google

--- js/PlanHW.js
+++ js/PlanHW.js
@@ -483,12 +483,14 @@ angular.module('PlanHW', ['ngRoute','ui.bootstrap.datetimepicker','webStorageMod
         }
         $scope.gsigninURL = 
                 "https://accounts.google.com/o/oauth2/auth?" + [
-                    "scope=" + encodeURIComponent(['profile','email'].join(' ')),
+                    "scope=" + encodeURIComponent(['openid','email'].join(' ')),
                     "state=" + encodeURIComponent('google'),
                     "redirect_uri=" + encodeURIComponent(PlanHWApi+'oauth2callback'),
-                    "response_type=code&",
+                    "response_type=code",
+                    "prompt=consent",
                     "client_id=" + encodeURIComponent("179836333485-a9u3omrs9o0c1ik00fesa2043q0f63fe.apps.googleusercontent.com")
                 ].join('&')
+        console.log($scope.gsigninURL)
     })
     .controller('ForgotPassCtrl',function($scope, $http, $rootScope, $location){
         $scope.changePass = function(){
